c693fe5fe558fbff8597b06d67d37532ccf07b7b
Pengpa0012 <peng.ilikha@gmail.com>
2024-02-18 21:56:32 +0800

    added latest mangas

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Test")
+
+
+const cardsCover = document.querySelector(".cards")
+
+fetch(API_URL, {
+  headers: {
+    'X-RapidAPI-Key': KEY,
+    'X-RapidAPI-Host': HOST
+  }
+})
+.then(res => res.json())
+.then(data => {
+  const mangas = data.data
+
+  mangas.forEach(el => {
+    const newImg = document.createElement("img")
+    newImg.src = el.thumb
+    newImg.style.width = "200px"
+    newImg.style.height = "300px"
+    cardsCover.appendChild(newImg)
+  });
+  
+})
+
