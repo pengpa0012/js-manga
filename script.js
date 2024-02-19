ac4d7c4d1dbad3ddf79ff531f37ab97006bd0e98
Pengpa0012 <peng.ilikha@gmail.com>
2024-02-19 08:13:35 +0800

    update cards

--- script.js
+++ script.js
@@ -13,12 +13,18 @@ fetch(API_URL, {
   const mangas = data.data
 
   mangas.forEach(el => {
-    const newImg = document.createElement("img")
-    newImg.src = el.thumb
-    newImg.style.width = "200px"
-    newImg.style.height = "300px"
-    cardsCover.appendChild(newImg)
+    const newCard = document.createElement("div")
+      newCard.className = "shadow-md rounded-md overflow-hidden w-[200px]"
+      newCard.innerHTML = `
+      <img src="${el.thumb}" class="w-full h-[300px] mb-1 object-cover">
+      <div class="px-2 pb-2">
+        <h3 class="text-sm truncate w-[190px]">${el.title}</h3>
+        <p class="text-xs mb-1">${el.authors[0]}</p>
+        <p class="text-xs">Total Chapters: ${el.total_chapter}</p>
+      </div>
+      `
+    cardsCover.appendChild(newCard)
   });
-  
 })
+
 
