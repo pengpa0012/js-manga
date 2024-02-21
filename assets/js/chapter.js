f8423f16181edd0b31a0568346ff94b530112b85
Pengpa0012 <peng.ilikha@gmail.com>
2024-02-21 09:29:13 +0800

    added fetch images

--- assets/js/chapter.js
+++ assets/js/chapter.js
@@ -1,2 +1,14 @@
+import { fetchData } from "./utility.js"
+const images = document.querySelector(".images")
 const id = window.location.search.split("=")[1]
-console.log(id)
+
+// onMount
+fetchData("/manga/image", `?id=${id}`).then(data => {
+    data.forEach(el => {
+      const newImg = document.createElement("img")
+      newImg.src = el.link
+      newImg.className = "w-full"
+      images.appendChild(newImg)
+    })
+})
+
